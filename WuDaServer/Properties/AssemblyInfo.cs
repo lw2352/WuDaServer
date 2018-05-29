fa58beb7254557cb22edadcbf7212e864bab891d
wei li <liweiliwei0103@outlook.com>
2018-05-29 16:41:00 +0800

    更改为winform项目类型

--- WuDaServer/Properties/AssemblyInfo.cs
+++ WuDaServer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("1f39d638-776a-4a9c-8012-54d1fa68fa99")]
+[assembly: Guid("abf458a4-2432-46f3-a977-8cbdd9046ae9")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
