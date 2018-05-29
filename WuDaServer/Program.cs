3a9b92f313c0b847b8faded77e4d6cb1ed9aa78c
wei li <liweiliwei0103@outlook.com>
2018-05-29 16:25:28 +0800

    添加项目文件。

---
+++ WuDaServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WuDaServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
