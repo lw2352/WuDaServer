a1877e5e6323ec98dc7e86e312cdd2305698f2e2
wei li <liweiliwei0103@outlook.com>
2018-06-28 16:26:16 +0800

    fix bugs（重复的命令）

--- WuDaServer/MainUdpClass.cs
+++ WuDaServer/MainUdpClass.cs
@@ -433,7 +433,7 @@ public class MainUdpClass
                                     //设置升级属性
                                     dataItem.tUpdate.IsNeedUpdate = true;
                                 }
-                                else //普通指令可以直接构造并发送
+                                else if(cmdStrings[i, 1] != "search")//普通指令可以直接构造并发送
                                 {
                                     byte[] cmd = CmdClass.makeCommand(cmdStrings[i, 1], cmdStrings[i, 2],
                                         cmdStrings[i, 3],
